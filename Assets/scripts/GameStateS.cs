066910807b295ca1a4945eccb1737af5b142861d
Ohotnik <volodyaohana@gmail.com>
2023-07-09 11:39:02 +0200

    Added
    level of dif

--- Assets/scripts/GameStateS.cs
+++ Assets/scripts/GameStateS.cs
@@ -9,7 +9,8 @@ public class GameStateS : MonoBehaviour
     public bool SoundIsOn = true;
     public bool MusicIsOn = true;
 
-    
+    public int LevelOfDif;
+
 
     // Update is called once per frame
     void Update()
@@ -28,5 +29,18 @@ public class GameStateS : MonoBehaviour
     public void ChangeMusicState()
     {
         MusicIsOn = !MusicIsOn;
+    }
+
+    public void SetDifToEase()
+    {
+        LevelOfDif = 0;
+    }
+    public void SetDifNormal()
+    {
+        LevelOfDif = 1;
+    }
+    public void SetDifToHard()
+    {
+        LevelOfDif = 2;
     }
 }
